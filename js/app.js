aaf3ff26d4aef8f308b5dc888a94b642eeae1ca1
CharlotteB25 <charbill@student.arteveldehs.be>
2022-06-20 16:23:12 +0200

    20.06

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 let miner = document.querySelector('.miner');
-let move = 100;
+let move = 50;
 
-window.addEventListener('load', () =>{
+window.addEventListener('load', () => {
 	miner.style.position = 'absolute';
 	miner.style.left = 0;
 	miner.style.top = 0;
 });
 
 window.addEventListener('keyup', (e) =>{
-	switch(e.key){
-		case 'arrowLeft' :
+	switch(e.key) {
+		case 'ArrowLeft' :
 			miner.style.left = parseInt(miner.style.left) - move + 'px';
 			break;
-		case 'arrowRight' :
-			miner.style.left = parseInt(miner.style.left) - move + 'px';
+		case 'ArrowRight' :
+			miner.style.left = parseInt(miner.style.left) + move + 'px';
 			break;
-		case 'arrowUp' :
+		case 'ArrowUp' :
 			miner.style.top = parseInt(miner.style.top) - move + 'px';
 			break;
-		case 'arrowDown' :
-			miner.style.top = parseInt(miner.style.top) - move + 'px';
+		case 'ArrowDown' :
+			miner.style.top = parseInt(miner.style.top) + move + 'px';
 			break;
 	}
 });
