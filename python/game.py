48896c6a70fa837df89e6451d0e15eaaada8dfe1
CharlotteB25 <charbill@student.arteveldehs.be>
2022-06-21 09:02:24 +0100

    21.06

---
+++ python/game.py
@@ -0,0 +1,13 @@
+from flask import flask
+import requests 
+from sense_hat import SenseHat
+from time import sleep
+from random import randint
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "hello, flask"
+
+def move(direction):
+    if direction == 'left':
